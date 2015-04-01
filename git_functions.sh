f02facfba66b54efdf41bba092130866baeb42ba
Alberto Scotto <scotto.alberto.86@gmail.com>
2015-04-01 23:54:28 +0100

    extract functions from git_pull_safe to git_functions.sh

---
+++ git_functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+git_current_branch() {
+  git rev-parse --abbrev-ref HEAD
+}
+
+git_save_state() {
+  ORIGINAL_BRANCH=$( git_current_branch )
+  stash_output=`git stash`
+  # TODO improve "did an err occur?" logic -- should not be based on the exact string
+  if [ "$stash_output" = "No local changes to save" ] ; then
+    STASHED=0
+  else
+    STASHED=1
+  fi
+}
+
+git_restore_state() {
+  ret=0 # assume success
+  if [ -n "$ORIGINAL_BRANCH" ] ; then
+    git co "$ORIGINAL_BRANCH"
+  fi
+  if [ "$STASHED" -eq 1 ] ; then
+    git stash pop
+    ret=$?
+  fi
+  STASHED=0
+  return $ret
+}
+
